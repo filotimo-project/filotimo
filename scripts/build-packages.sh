25b486212e6cd02ac8175e56fc2ea51748f58053
Thomas Duckworth <tduck973564@gmail.com>
2024-12-19 21:45:51 +1100

    try nspawn for building packages

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -25,14 +25,14 @@ for dir in */; do
         spectool -g -R "$specfile" --define "_sourcedir $(pwd)" \
             || { echo "failed to download sources for $dir"; exit 1; }
 
-        mock --isolation=simple --define "debug_package %{nil}" \
+        mock --define "debug_package %{nil}" \
             --buildsrpm \
             --spec "$specfile" \
             --sources . \
             --resultdir=/tmp/packages/srpms/$dir \
             || { echo "failed to build srpm for $dir"; exit 1; }
 
-        mock --isolation=simple --define "debug_package %{nil}" \
+        mock --define "debug_package %{nil}" \
             --rebuild /tmp/packages/srpms/$dir/*.src.rpm \
             --resultdir=/tmp/packages/rpms/$dir \
             || { echo "failed to build rpm for $dir"; exit 1; }
