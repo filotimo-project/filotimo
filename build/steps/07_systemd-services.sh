b18a8b4f7c154b67e046340991a00fca277b8f0d
Thomas Duckworth <tduck@filotimoproject.org>
2025-06-14 19:42:43 +1000

    fix: remove nonexistent services

--- build/steps/07_systemd-services.sh
+++ build/steps/07_systemd-services.sh
@@ -10,9 +10,6 @@ systemctl mask hibernate.target
 # Workaround immutable SDDM theme directory
 systemctl enable usr-share-sddm-themes.mount
 
-# Configure the firefox flatpak
-systemctl enable firefox-flatpak-configuration.path
-
 # Services
 systemctl --global enable check-windows-dualboot.service
 systemctl enable smb.service
@@ -20,7 +17,6 @@ systemctl enable adbusers-append.service
 systemctl enable dirty-centisecs.service
 systemctl enable memory-tweaks.service
 systemctl enable plugdev-append.service
-systemctl enable postinstall-hardware-setup.service
 systemctl enable set-grub-visibility.service
 systemctl disable flatpak-add-fedora-repos.service
 
