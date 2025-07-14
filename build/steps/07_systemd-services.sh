1315a2323d694e3c008dcfb77bec26c68ad0d7ed
Thomas Duckworth <tduck@filotimoproject.org>
2025-07-14 23:19:14 +1000

    fix: bring back ublue-system-setup workaround

--- build/steps/07_systemd-services.sh
+++ build/steps/07_systemd-services.sh
@@ -23,5 +23,7 @@ systemctl enable ublue-os-libvirt-workarounds.service
 systemctl disable flatpak-add-fedora-repos.service
 
 # Setup hook runners
+# FIXME: workaround for it being fucked upstream
+sed -i '/^\[Service\]$/a User=root' /usr/lib/systemd/system/ublue-system-setup.service
 systemctl enable ublue-system-setup.service
 systemctl --global enable ublue-user-setup.service
